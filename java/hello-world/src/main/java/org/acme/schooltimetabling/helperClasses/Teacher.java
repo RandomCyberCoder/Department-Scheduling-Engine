63ec444f39f8d531f5fb5d71b5adffc9d59a0be7
RandomCyberCoder <emmanueldelgado74@gmail.com>
2025-02-12 12:48:32 -0800

    added teacher helper class

--- java/hello-world/src/main/java/org/acme/schooltimetabling/helperClasses/Teacher.java
+++ java/hello-world/src/main/java/org/acme/schooltimetabling/helperClasses/Teacher.java
@@ -1,2 +1,20 @@
-package org.acme.schooltimetabling.helperClasses;public class Teacher {
+package org.acme.schooltimetabling.helperClasses;
+
+import java.util.List;
+import java.util.BitSet;
+
+public class Teacher {
+    /*we should store canon name here*/
+    public String name;
+
+    /*timeslot preferences*/
+    public BitSet preferences;
+
+    /*acceptable timeslots*/
+    public BitSet acceptable;
+
+    /*impossible timeslots*/
+    public BitSet conflict;
+
+
 }
