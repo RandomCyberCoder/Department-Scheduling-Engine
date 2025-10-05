de4169bf35ae7adc72101701e7de5e1330680651
RandomCyberCoder <emmanueldelgado74@gmail.com>
2025-10-04 20:18:25 -0700

    feat: renamed endpoint urls and added a new one for faculty updates

--- backend/django_backend/scheduling_backend/api/urls.py
+++ backend/django_backend/scheduling_backend/api/urls.py
@@ -3,13 +3,15 @@ from .views.teacher_views import (
     get_users, 
     create_user,
     users_file_upload,
-    update_teacher
+    update_teacher,
+    set_faculty,
 )
 
 urlpatterns = [
     #route for api
-    path('users/', get_users, name='get_user'),
-    path('users/create/', create_user, name='create_user'),
-    path('users/<int:pk>/', update_teacher, name='update_teacher'),
-    path('users/file/', users_file_upload, name='users_file_upload'),
+    path('teachers/', get_users, name='get_user'),
+    path('teachers/create/', create_user, name='create_user'),
+    path('teachers/<int:pk>/', update_teacher, name='update_teacher'),
+    path('teachers/file/', users_file_upload, name='users_file_upload'),
+    path('teachers/faculty/file/', set_faculty, name='set_faculty'),
 ]
