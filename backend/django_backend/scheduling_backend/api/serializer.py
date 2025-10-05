a1f8cced7b5063dac06a9116ce336dfc7dc25d0f
RandomCyberCoder <emmanueldelgado74@gmail.com>
2025-10-04 22:44:40 -0700

    feat: added serializer for survey model

--- backend/django_backend/scheduling_backend/api/serializer.py
+++ backend/django_backend/scheduling_backend/api/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Teacher
+from .models import Teacher, Survey
 
 # convert and properly transport you data into json your api can use?
 class TeacherSerializer(serializers.ModelSerializer):
@@ -7,10 +7,15 @@ class TeacherSerializer(serializers.ModelSerializer):
         model = Teacher
         fields = '__all__'
 
-class FileUpload(serializers.Serializer):
+class FileUploadSerializer(serializers.Serializer):
     file = serializers.FileField()
 
 class DepartmentFileUpload(serializers.Serializer):
     file = serializers.FileField()
     department = serializers.CharField(max_length=3)
+
+class SurveySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Survey
+        fields = '__all__'
 
