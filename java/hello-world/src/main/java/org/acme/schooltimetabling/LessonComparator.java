243f258d40d7eec52e2a8d9b9fcb18f5d9efc79d
RandomCyberCoder <emmanueldelgado74@gmail.com>
2026-02-04 13:04:23 -0800

    feat: starter code for implementing new algos

---
+++ java/hello-world/src/main/java/org/acme/schooltimetabling/LessonComparator.java
@@ -0,0 +1,13 @@
+package org.acme.schooltimetabling;
+
+import org.acme.schooltimetabling.domain.Lesson;
+
+import java.util.Comparator;
+
+public class LessonComparator implements Comparator<Lesson> {
+    public int compare(Lesson ls1, Lesson ls2){
+        //descending order
+        //we want things with more lab hours to have scheduling priority
+        return Integer.compare(ls2.getLab_activity_hours(), ls1.getLab_activity_hours());
+    }
+}
