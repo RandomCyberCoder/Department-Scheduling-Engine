d33d0edd9d36adfeb48b5cc78155123c306f5a04
RandomCyberCoder <emmanueldelgado74@gmail.com>
2026-02-20 13:50:30 -0800

    feat: determinig what lessons have higher priority

--- java/hello-world/src/main/java/org/acme/schooltimetabling/domain/lesson/LessonComparator.java
+++ java/hello-world/src/main/java/org/acme/schooltimetabling/domain/lesson/LessonComparator.java
@@ -1,13 +1,11 @@
 package org.acme.schooltimetabling.domain.lesson;
 
-import org.acme.schooltimetabling.domain.lesson.Lesson;
-
 import java.util.Comparator;
 
 public class LessonComparator implements Comparator<Lesson> {
     public int compare(Lesson ls1, Lesson ls2){
         //descending order
         //we want things with more lab hours to have scheduling priority
-        return Integer.compare(ls2.getLab_activity_hours(), ls1.getLab_activity_hours());
+        return Integer.compare(ls2.getLabActHours(), ls1.getLabActHours());
     }
 }
